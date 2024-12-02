2ba810fd946e906fb0b715a839e3f336d0905d2f
Aarón Villalta <73033754+gacvillalta34@users.noreply.github.com>
2024-12-02 17:24:29 -0600

    Update sms.js

--- assets/js/sms.js
+++ assets/js/sms.js
@@ -9,34 +9,12 @@ document.getElementById('form')
    const serviceID = 'default_service';
    const templateID = 'template_h02127e';
 
-   const showModal = (title, message, isSuccess = true) => {
-    const modal = document.getElementById('customModal');
-    const modalTitle = document.getElementById('modalTitle');
-    const modalMessage = document.getElementById('modalMessage');
-    const modalCloseBtn = document.getElementById('modalCloseBtn');
-  
-    // Configurar contenido del modal
-    modalTitle.textContent = title;
-    modalMessage.textContent = message;
-    modalTitle.style.color = isSuccess ? '#4CAF50' : '#f44336'; // Verde para éxito, rojo para error
-    modalCloseBtn.style.backgroundColor = isSuccess ? '#4CAF50' : '#f44336';
-  
-    // Mostrar modal
-    modal.style.display = 'block';
-  
-    // Cerrar modal al hacer clic en el botón
-    modalCloseBtn.onclick = () => {
-      modal.style.display = 'none';
-    };
-  };
-  
-  emailjs.sendForm(serviceID, templateID, this)
+   emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
-      showModal('Success!', 'Your email has been sent successfully.');
+      alert('Sent!');
     }, (err) => {
       btn.value = 'Send Email';
-      showModal('Error', `Failed to send email. Details: ${JSON.stringify(err)}`, false);
+      alert(JSON.stringify(err));
     });
-  
 });
